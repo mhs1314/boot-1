604857b73f35e2924770662f7586524d61a22b51
mhs1314 <471002385@qq.com>
2018-03-06 09:59:37 +0800

    更改controller -> page包

--- boot-1/src/main/java/com/example/demo/page/HelloSpringbootController.java
+++ boot-1/src/main/java/com/example/demo/page/HelloSpringbootController.java
@@ -1,4 +1,4 @@
-package com.example.demo.contorller;
+package com.example.demo.page;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
