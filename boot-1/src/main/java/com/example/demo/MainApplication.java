6fb4b996a07703b67434ff4de485d63f63c9d791
mhs1314 <471002385@qq.com>
2018-03-06 10:16:39 +0800

    将spring boot 入口文件更改为main以方便理解

--- boot-1/src/main/java/com/example/demo/MainApplication.java
+++ boot-1/src/main/java/com/example/demo/MainApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Boot1Application {
+public class MainApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Boot1Application.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 }
