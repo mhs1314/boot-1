676fbed40459dfe4f0f20484d1fece4bacc4cce0
mhs1314 <471002385@qq.com>
2018-03-06 18:04:59 +0800

    xx

--- boot-1/src/main/java/com/example/demo/MainApplication.java
+++ boot-1/src/main/java/com/example/demo/MainApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//mybaits扫描dao类路径
 @MapperScan("com.example.demo.data.*.dao")
 @SpringBootApplication
 public class MainApplication  {
