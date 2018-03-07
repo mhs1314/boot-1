42c3715dde09cbfe9184e92d11f12619ddf1583b
Administrator <Administrator@YLMF-1704081533>
2018-03-08 00:10:07 +0800

    页面freemarker魔板,security引入

--- boot-1/src/main/java/com/example/demo/factory/user/serviceImpl/UserServiceImpl.java
+++ boot-1/src/main/java/com/example/demo/factory/user/serviceImpl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.factory.user.serviceImpl;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
--- boot-1/src/main/java/com/example/demo/page/user/UserController.java
+++ boot-1/src/main/java/com/example/demo/page/user/UserController.java
@@ -5,10 +5,12 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.factory.user.service.UserService;
+
 
 @Controller
 @RequestMapping("user")
@@ -17,9 +19,10 @@ public class UserController {
 	private UserService userService;
 
 	@RequestMapping("/index")
-	public String index() {
+	public String index(Model model) {
 		//使用@restController注解shi
 	//	ModelAndView mv = new ModelAndView("index");
+	    model.addAttribute("name", "Dear");
 		return "index";
 	}
 
